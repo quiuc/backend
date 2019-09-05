772bfdc219d0b5a4aaaabfae8b354735e8b93994
Bailey Tincher <btincher99@gmail.com>
2019-09-05 11:40:26 -0500

    Adding backend webmailer

---
+++ mailer.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const middy = require('middy');
+const {
+  cors
+} = require('middy/middlewares');
+
+const nodemailer = require('nodemailer');
+
+const handler = async (event) => {
+  try {
+    
+    const user = JSON.parse(event.body);
+
+    if (!(user.name || user.email || user.message)) {
+      return {
+        statusCode: 400,
+        body: 'Invalid schema.'
+      }
+    }
+
+    const message = `Name: ${user.name}\nEmail: ${user.email}\n\nMessage: ${user.message}`;
+  
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: process.env.EMAIL, 
+          pass: process.env.EMAIL_PASSWORD 
+      }
+    });
+  
+    await transporter.sendMail({
+      from: '"QUIUC Website Mailer" <quiucgeneral@gmail.com>',
+      to: 'info@quiuc.org',
+      subject: `New Message from '${user.email}'`,
+      text: message, 
+    });
+
+    return {
+      statusCode: 200
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: err
+    };
+  }
+
+};
+
+module.exports.handler = middy(handler)
+  .use(cors());
